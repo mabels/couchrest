2524f69b32d26436a33082ddf7f508159bdde5ea
Meno Abels <meno@abels.name>
2010-03-17 19:42:56 +0100

    version stepup

--- lib/couchrest.rb
+++ lib/couchrest.rb
@@ -28,7 +28,7 @@ require 'couchrest/monkeypatches'
 
 # = CouchDB, close to the metal
 module CouchRest
-  VERSION    = '1.10' unless self.const_defined?("VERSION")
+  VERSION    = '1.11' unless self.const_defined?("VERSION")
   
   autoload :Server,       'couchrest/core/server'
   autoload :Database,     'couchrest/core/database'
